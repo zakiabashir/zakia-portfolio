63211edd9ff6a345a602e8778265f9523ece3b0c
zakiabashir <nshafeys0@gmail.com>
2024-10-16 07:58:22 +0500

    push my code

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <div className="bg-blue-50 z-50 sticky bottom-0">
       <footer className="text-gray-600 body-font">
-        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <div className="container px-5 py-2 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <Image
               src={"/assets/images/zlogo.jpeg"}
